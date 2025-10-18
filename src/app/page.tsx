25f245896d1949232c2eeeb2d4d59541cefbc7df
Explorarthecreator <fredadejiemma99@gmail.com>
2025-10-18 14:46:17 +0100

    feat: created the knowledge section for every week of new content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import MissionAndVission from "@/components/section/about-us";
 import Community from "@/components/section/community";
 import GetInTouch from "@/components/section/get-in-touch";
+import Knowledge from "@/components/section/knowledge";
 import MainSection from "@/components/section/main-section";
 import Testimonial from "@/components/section/testimonials";
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <div>
       <MainSection />
+      <Knowledge />
       <Community />
       <div id="about-us" className="pt-[100px]"></div>
       <MissionAndVission />
